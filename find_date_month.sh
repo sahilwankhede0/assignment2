007a54ba5838bd1f1a874efd8c4d059e0d7da056
sahil <sahilwankhede0@gmail.com>
2022-05-07 09:00:34 +0530

    added the find_date_month file

---
+++ find_date_month.sh
@@ -0,0 +1,12 @@
+echo "Write a program that takes day and month from the command line and prints true  if day of month is between March 20 and June 20, false otherwise."
+
+read -p " Enter Date:-" date
+read -p " Enter Month:-" Month
+
+if (( ((($Month >= 3 && $date <= 20) && $Month <= 6 && $date <= 20)  && ($date<31)) ))
+then
+        echo "this month is " $Month "and date is " $date "thats True";
+else
+
+        echo "False";
+fi
